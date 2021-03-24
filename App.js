2c20cafeb206608c6cb07dd3b48102b7d442dc36
Anam Hussain <anamhussain2601@gmail.com>
2021-03-25 00:47:03 +0530

    adding text field and style

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const[outputText, setOutputText] = useState('Open up App.js to start working on you r app!')
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change Text" onPress={()=>setOutputText('The Text Changed!')}/>
+    <View style={{padding: 80}} >
+       <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems:'center'}}  >
+        <TextInput placeholder='Course Goal' style={{ width:'80%',  borderColor:'black', borderWidth: 1}}/>
+        <Button title='ADD' />
+      </View>
+      <View >
+      
+      </View>
+      
     </View>
   );
 }
